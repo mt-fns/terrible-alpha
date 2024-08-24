f43e47ac3c1b260cd8d56168b65a55fcae1d96c8
Osvaldo Prajitno <z5473276@ad.unsw.edu.au>
2024-08-24 15:07:19 +0700

    tested openAI text to speech

--- alpha-project/src/App.jsx
+++ alpha-project/src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 import './App.css'
 import Dictaphone from './components/Dictaphone'
 import Brain_Damage from './components/Brain_Damage'
+<<<<<<< HEAD
 
+=======
+>>>>>>> 7c6fbc3 (tested openAI text to speech)
 
 function App() {
   const [count, setCount] = useState(0)
