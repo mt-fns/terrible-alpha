518d52604567aff2acae250906bf35a3836ac859
Michael Tanto <michaeltanto0909@gmail.com>
2024-08-24 14:31:48 +0700

    Connected speech recognition to API

--- alpha-project/backend/backend.js
+++ alpha-project/backend/backend.js
@@ -1,18 +1,21 @@
 import OpenAI from "openai";
 
-const openai = new OpenAI();
-
-
-async function main() {
+async function callAPI(prompt) {
+    const openai = new OpenAI({apiKey: "YOUR-API-KEY", dangerouslyAllowBrowser: true});
+    let output = "";
     const stream = await openai.chat.completions.create({
         model: "gpt-4o-mini",
         messages: [{ role: "user", content: "can you include chat in every prompt: can you translate this to brainrot (ohio rizz, chat and stuff) (make it 2 or 3 sentences) : i want some milk but daddy" }],
         stream: true,
     });
+
     for await (const chunk of stream) {
-        // console.log(chunk.choices[0]?.delta?.content || "");
-        process.stdout.write(chunk.choices[0]?.delta?.content || "");
+        output += chunk.choices[0]?.delta?.content || "";
     }
+
+    return output;
 }
 
-main();
+export {
+    callAPI
+}
