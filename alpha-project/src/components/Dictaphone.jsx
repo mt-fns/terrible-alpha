34ccae3e6fbb279f7d1afe6f22a8e4b5a7b7eeab
Jesslyn <z5455953@ad.unsw.edu.au>
2024-08-25 11:31:18 +1000

    combines front end and backend

--- alpha-project/src/components/Dictaphone.jsx
+++ alpha-project/src/components/Dictaphone.jsx
@@ -18,6 +18,7 @@ const Dictaphone = () => {
 
   const handleMicOff = () => {
     SpeechRecognition.stopListening();
+    setResultPending(true);
     callAPI(transcript).then(results => {
       textToSpeech(results).then(objectUrl => {
           const audio = new Audio(objectUrl);
@@ -41,9 +42,9 @@ const Dictaphone = () => {
         <img id="mic-off" src="/mic-off.png"/>
       }
 
-      <button class="buttons" id="start-button" onClick={SpeechRecognition.startListening}>Start</button>
-      <button class="buttons" id="stop-button" onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button class="buttons" id="reset-button" onClick={resetTranscript}>Reset</button>
+      <button className="buttons" id="start-button" disabled={resultPending} onClick={handleMicOn}>Start</button>
+      <button className="buttons" id="stop-button" onClick={handleMicOff}>Stop</button>
+      <button className="buttons" id="reset-button" onClick={resetTranscript}>Reset</button>
       <p id="speech-to-text">{transcript}</p>
       <p id="text-to-speech">the fitnessgram Pacer test is a multi-stage Arabic capacity test that progressively gets more difficult as a continuous a 20 metre pays or test will begin in 30 seconds line up at the start the running speed starts slowly but gets faster instrumented after you hear the signal a single app should be completed each time you hear the sound remember to run in a straight line and run as long as possible the second time you feel to complete a lab before the sound it is it's over to this will begin when the world starts on your market set ready start</p>
     </div>
