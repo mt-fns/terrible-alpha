263da1a58793f9b3ef8e9019c26f6e591fea31c7
Jesslyn <z5455953@ad.unsw.edu.au>
2024-08-25 13:55:55 +1000

    Removed token fr

--- alpha-project/src/components/Dictaphone.jsx
+++ alpha-project/src/components/Dictaphone.jsx
@@ -4,6 +4,7 @@ import { callAPI } from '../../backend/backend';
 import { textToSpeech } from '../../backend/text_to_speech';
 const Dictaphone = () => {
   const [resultPending, setResultPending] = useState(false);
+  const [apiResult, setApiResult] = useState("");
 
   const {
     transcript,
@@ -13,6 +14,8 @@ const Dictaphone = () => {
   } = useSpeechRecognition();
 
   const handleMicOn = () => {
+    resetTranscript();
+    setApiResult("");
     SpeechRecognition.startListening();
   }
 
@@ -20,14 +23,19 @@ const Dictaphone = () => {
     SpeechRecognition.stopListening();
     setResultPending(true);
     callAPI(transcript).then(results => {
+      setApiResult(results);
       textToSpeech(results).then(objectUrl => {
           const audio = new Audio(objectUrl);
           audio.play();
           setResultPending(false);
-          resetTranscript();
         }
       )
     })
+  }
+
+  const handleReset = () => {
+    setApiResult("");
+    resetTranscript();
   }
 
   if (!browserSupportsSpeechRecognition) {
@@ -44,9 +52,9 @@ const Dictaphone = () => {
 
       <button className="buttons" id="start-button" disabled={resultPending} onClick={handleMicOn}>Start</button>
       <button className="buttons" id="stop-button" onClick={handleMicOff}>Stop</button>
-      <button className="buttons" id="reset-button" onClick={resetTranscript}>Reset</button>
+      <button className="buttons" id="reset-button" onClick={handleReset}>Reset</button>
       <p id="speech-to-text">{transcript}</p>
-      <p id="text-to-speech">the fitnessgram Pacer test is a multi-stage Arabic capacity test that progressively gets more difficult as a continuous a 20 metre pays or test will begin in 30 seconds line up at the start the running speed starts slowly but gets faster instrumented after you hear the signal a single app should be completed each time you hear the sound remember to run in a straight line and run as long as possible the second time you feel to complete a lab before the sound it is it's over to this will begin when the world starts on your market set ready start</p>
+      <p id="text-to-speech">{apiResult}</p>
     </div>
   );
 };
