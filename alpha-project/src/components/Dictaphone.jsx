1233aa880a8a3b3ba7b1210681188449f3f170a3
Jesslyn <z5455953@ad.unsw.edu.au>
2024-08-25 11:21:51 +1000

    added ui

--- alpha-project/src/components/Dictaphone.jsx
+++ alpha-project/src/components/Dictaphone.jsx
@@ -15,9 +15,6 @@ const Dictaphone = () => {
 
   return (
     <div>
-      {/* <p id="microphone-text" >Microphone: {listening ? 'on' : 'off'}</p> */}
-      {/* <img id="mic-image" src={listening ? "./mic-on.png" : "./mic-off.png"}/> */}
-      {/* <p id={listening ? 'mic-on' : 'mic-off'}>njdknsjfak</p> */}
 
       {listening ? 
         <img id="mic-on" src="/mic-on.png"/>
@@ -29,6 +26,7 @@ const Dictaphone = () => {
       <button class="buttons" id="stop-button" onClick={SpeechRecognition.stopListening}>Stop</button>
       <button class="buttons" id="reset-button" onClick={resetTranscript}>Reset</button>
       <p id="speech-to-text">{transcript}</p>
+      <p id="text-to-speech">the fitnessgram Pacer test is a multi-stage Arabic capacity test that progressively gets more difficult as a continuous a 20 metre pays or test will begin in 30 seconds line up at the start the running speed starts slowly but gets faster instrumented after you hear the signal a single app should be completed each time you hear the sound remember to run in a straight line and run as long as possible the second time you feel to complete a lab before the sound it is it's over to this will begin when the world starts on your market set ready start</p>
     </div>
   );
 };
